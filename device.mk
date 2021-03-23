5d0205bf538a077200bd8d62b45286aa96128d7b
ceb <henloboii@protonmail.ch>
2021-03-23 11:46:44 +0800

    sweet: add prebuilt google sounds

--- device.mk
+++ device.mk
@@ -28,6 +28,10 @@ PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
+# Prebuilt Google Sounds
+PRODUCT_PACKAGES += \
+    PrebuiltGoogleSounds
+
 # Properties
 include $(LOCAL_PATH)/vendor_prop.mk
 
